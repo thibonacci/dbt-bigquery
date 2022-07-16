fe4277aa06dd7374432f1eba1611de54319794ab
Thibault He <thibault.he@onfido.com>
2022-07-16 19:16:45 +0100

    correct windowing

--- gocardless/target/compiled/gocardless/analyses/data_exploration/payments_metadata/payments_amount.sql
+++ gocardless/target/compiled/gocardless/analyses/data_exploration/payments_metadata/payments_amount.sql
@@ -2,13 +2,24 @@ with payments as (
 
     select * from `gc-prd-ext-data-test-prod-906c`.`gc_paysvc_live`.`payments`
 
+),
+
+windowing as (
+
+    select 
+
+        currency,
+        min(amount) over(partition by currency) as min_amount,
+        max(amount) over(partition by currency) as max_amount,
+        percentile_cont(amount, 0.5) over(partition by currency) as median
+
+    from payments
+
 )
 
-select 
+select
+    *
 
-    currency,
-    min(amount) over() as min_amount,
-    max(amount) over() as max_amount,
-    percentile_cont(amount, 0.5) over() as median
+from windowing 
 
-from payments
+group by 1,2,3,4
